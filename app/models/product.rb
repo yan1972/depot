3343df9a12d4eb71e599d2f2a509a449e89d2ce0
chris yan <yan972@yahoo.com>
2012-06-04 13:29:27 +0800

    catalog display

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,9 @@
 class Product < ActiveRecord::Base
+  
+  default_scope :order => 'title'
+  
   attr_accessible :description, :image_url, :price, :title
+  
   validates :description, :image_url, :title, :presence => true
   validates :price, :numericality => {:greater_than_or_equal_to => 0.01}
   validates :title, :uniqueness => true
